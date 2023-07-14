b36761514b5fb04ef9764e1f929abf9115dcc78e
Mos3aB696 <mos3ab696@gmail.com>
2023-07-15 00:03:57 +0300

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print numbers form 0 to 9
+ * Description: using the main function
+ * print numbers form 0 to 9
+ *Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+		printf("\n");
+	return (0);
+}
