08429bb808c9386e57b48349adf4c7e6792ba08b
Mos3aB696 <mos3ab696@gmail.com>
2023-07-19 17:00:28 +0300

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
  * print_last_digit - function that prints the last digit of a number
  * @ld: that will use for the argument of the function
  * Return: 0
-*/
+ */
 
 int print_last_digit(int ld)
 {
