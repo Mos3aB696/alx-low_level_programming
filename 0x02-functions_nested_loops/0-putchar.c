6481c00052aa0bd383bc9067e2a18c2003183459
Mos3aB696 <mos3ab696@gmail.com>
2023-07-19 03:30:14 +0300

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char holberton[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	char holberton[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	unsigned int c;
 
 	for (c = 0; c < sizeof(holberton); c++)
