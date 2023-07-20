077282cb341afdad8f8052797469c5c3139203fe
Mos3aB696 <mos3ab696@gmail.com>
2023-07-20 15:26:18 +0300

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,30 +7,19 @@ void times_table(void)
 {
 	int i, j, n;
 
-	for (i = 0; i <= 12; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0');
-		for (j = 1; j <= 12; j++)
+		for (j = 1; j <= 9; j++)
 		{
 			n = i * j;
 			_putchar(',');
 			_putchar(' ');
 			if (n <= 9)
-			{
 				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (n > 9 && n <= 99)
-			{
-				_putchar(' ');
-				_putchar((n / 10) + '0');
-			}
 			else
-			{
-				_putchar((n / 100) + '0');
-				_putchar(((n / 10) % 10) + '0');
-			}
-				_putchar((n % 10) + '0');
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 	}
