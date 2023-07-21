e8f56f7c144df118b3e3d993db0bec24ac80de38
Mos3aB696 <mos3ab696@gmail.com>
2023-07-21 15:57:34 +0300

    done

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,15 +6,18 @@
  */
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 	else
 	{
