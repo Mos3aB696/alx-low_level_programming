3d38ecd8d63d176ecc920d7300463dab2fa7f475
Mos3aB696 <mos3ab696@gmail.com>
2023-07-27 15:09:37 +0300

    done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strcat - function that concatenates two strings
+ * @dest: String One
+ * @src: String Two
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	size_t i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return dest;
+}
