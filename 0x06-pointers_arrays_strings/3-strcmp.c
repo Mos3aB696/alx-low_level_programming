4d191320fc5a7825689fe66ad74a95638393d8ad
Mos3aB696 <mos3ab696@gmail.com>
2023-07-27 21:33:22 +0300

    done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings
+ * @s1: String One
+ * @s2: String One
+ *
+ * Return: 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 > s2)
+		return (15);
+	else if (s1 < s2)
+		return (-15);
+	else
+		return (0);
+}
