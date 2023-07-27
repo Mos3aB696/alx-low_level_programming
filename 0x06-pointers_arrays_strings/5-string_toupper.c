e9d36a3dbddc8ee2f9cffcf25da5b216600f0e82
Mos3aB696 <mos3ab696@gmail.com>
2023-07-28 01:19:09 +0300

    done

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -18,4 +18,14 @@ char *string_toupper(char *str)
 		i++;
 	}
 	return (str);
+	/**
+	* char *ptr = str;
+	* while (*str)
+	* {
+	* if (*str >= 'a' && *str <= 'z')
+	* *str -= 32;
+	* str++;
+	* }
+	* return (ptr);
+	*/
 }
