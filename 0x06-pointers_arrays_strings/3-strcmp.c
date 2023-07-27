5e1a327ca4fdb9306e6d8f7c35944d8456cda041
Mos3aB696 <mos3ab696@gmail.com>
2023-07-27 21:41:00 +0300

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 > s2)
-		return (15);
-	else if (s1 < s2)
-		return (-15);
-	else
-		return (0);
+	while (*s1 && *s2)
+	{
+		if (*s1 - *s2 != 0)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (0);
 }
