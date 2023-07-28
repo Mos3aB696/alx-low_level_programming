d059d597c38a73340c830baf2d4503e5ef9cc116
Mos3aB696 <mos3ab696@gmail.com>
2023-07-28 03:07:39 +0300

    done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,8 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
+char *string_toupper(char *src);
+char *cap_string(char *src);
+char *leet(char *src);
 
 #endif /* MAIN_H */
