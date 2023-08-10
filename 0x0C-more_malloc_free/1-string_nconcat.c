b163d230859b6b2d32ab094b054e002ffa67d937
Mos3aB696 <mos3ab696@gmail.com>
2023-08-11 01:18:07 +0300

    done

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * *string_nconcat - concatenates two strings.
+ * @s1: stringOne
+ * @s2: stringTwo
+ * @n: numbers of lenght to concat
+ *
+ * Return: ptr
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	/* get length (s1, s2) */
+	unsigned int lenOne = strlen(s1), lenTwo = strlen(s2);
+	/* for loop */
+	unsigned int i, j;
+	char *ptr;
+	/*check if equal NULL*/
+	if (s1 == NULL || s2 == NULL)
+		return ("");
+	/*check if n >= lenTwo*/
+	if (lenTwo <= n)
+	{
+		n = lenTwo;
+		ptr = malloc(sizeof(char) * (lenOne + n + 1));
+	}
+	else
+		ptr = malloc(sizeof(char) * (lenOne + n + 1));
+	/*check if ptr == NULL*/
+	if (ptr == NULL)
+		return (NULL);
+	/*first loop to store s1 values in ptr*/
+	for (i = 0; i < lenOne; i++)
+		ptr[i] = s1[i];
+	/*second loop to store s2 values in ptr*/
+	for (j = 0; j < n; j++)
+		ptr[i++] = s2[j];
+	/*add null in the end*/
+	ptr[i++] = '\0';
+	return (ptr);
+}
