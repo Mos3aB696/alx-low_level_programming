348c3df9c0ac9ca1b8a4b9a0b57c957bfe56868a
Mos3aB696 <mos3ab696@gmail.com>
2023-08-14 15:32:05 +0300

    done

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,13 +4,21 @@
  * @d: the new haw haw
  *
  * Return: void
-*/
+ */
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		(d->name) ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+
 		printf("age: %f\n", d->age);
-		(d->owner) ? printf("Owner: %s\n", d->owner) : printf("Owner:(nil)\n");
+
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner:(nil)\n");
 	}
 }
