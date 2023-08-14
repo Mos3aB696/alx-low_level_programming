a93ffa8802bf2abff2c475e35f244979ba4f13b6
Mos3aB696 <mos3ab696@gmail.com>
2023-08-14 18:01:38 +0300

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 /**
- * *new_dog - creates a new dog
+ * new_dog - creates a new dog
  * @name: name of haw haw
  * @age: age of haw haw
  * @owner: name of owner of haw haw
@@ -15,8 +15,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (ptr == NULL)
 		return (NULL);
 
-	ptr->name = name;
+	ptr->name = malloc(strlen(name) + 1);
+	if (ptr->name != NULL)
+	{
+		strcpy(ptr->name, name);
+	}
+
 	ptr->age = age;
-	ptr->owner = owner;
+
+	ptr->owner = malloc(strlen(owner) + 1);
+	if (ptr->owner != NULL)
+	{
+		strcpy(ptr->owner, owner);
+	}
+
 	return (ptr);
 }
