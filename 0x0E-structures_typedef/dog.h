2f2ba0e07595a6fa57721a493223fd4d2150b4f3
Mos3aB696 <mos3ab696@gmail.com>
2023-08-14 16:43:05 +0300

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,13 @@
  * @age: age of haw haw
  * @owner: name of owner of haw haw
  */
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
