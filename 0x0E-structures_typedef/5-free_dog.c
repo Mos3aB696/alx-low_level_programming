c2992243f5beca9c1157db818eca50d6c1e4a7df
Mos3aB696 <mos3ab696@gmail.com>
2023-08-14 21:11:08 +0300

    done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * free_dog - frees dogs.
+ * @d: All Dogs
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
