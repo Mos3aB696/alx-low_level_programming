9b141b779d729946f123c1debc8ffbb3035e6801
Mos3aB696 <mos3ab696@gmail.com>
2023-08-14 10:32:39 +0300

    done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Haw Haw structure
+ * @name: name of haw haw
+ * @age: age of haw haw
+ * @owner: name of owner of haw haw
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
