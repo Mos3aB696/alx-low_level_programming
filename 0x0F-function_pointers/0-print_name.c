205c5929043489b341d205bdc945894d70874e6a
Mos3aB696 <mos3ab696@gmail.com>
2023-08-17 00:10:42 +0300

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name of user
+ * @f: the pointer function
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
