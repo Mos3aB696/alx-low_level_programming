46521d57a284c8dffd5ebebf0d0cfc796ca1df9c
Mos3aB696 <mos3ab696@gmail.com>
2023-08-17 02:06:18 +0300

    done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,18 +12,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
-	if (array != NULL && cmp != NULL)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		if (cmp(array[i]))
 		{
-			if (cmp(array[i]))
-			{
-				return (i);
-			}
+			return (i);
 		}
 	}
+
 	return (0);
 }
