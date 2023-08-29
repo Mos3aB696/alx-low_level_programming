eb865b2f3f9ed2d3bd0ca2d7638b0541ea02af46
Mos3aB696 <mos3ab696@gmail.com>
2023-08-29 19:17:10 +0300

    done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *data;
 
-	while (*head != NULL || head != NULL)
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
 	{
 		data = (*head)->next;
 		free(*head);
