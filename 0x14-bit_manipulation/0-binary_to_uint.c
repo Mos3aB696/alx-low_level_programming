791825515f1e4a2e9fadc06810cbe9fcdc2767bc
Mos3aB696 <mos3ab696@gmail.com>
2023-09-01 01:43:08 +0300

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,33 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i;
-	char *endptr;
-	unsigned int result;
+	int i;
+	unsigned int result = 0;
+
+	if (!b)
+		return (0);
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
+			/**
+		* result = 0000 0010
+							//0000 0001
+							//0000 0011
+							//0000 0110
+							//0000 1100
+							//0001 1000
+							//0011 0000
+							//0000 0001
+							//0011 0001
+							//0110 0010
+							//0 + 2 + 0 + 0 + 0 + 32 + 64
+			*/
+		result <<= 1;
+		if (b[i] == '1')
+			result += 1;
 	}
-	if (b == NULL)
-		return (0);
-
-	result = strtol(b, &endptr, 2);
 
 	return (result);
 }
