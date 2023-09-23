ba94f4bbb0a13a6d58bc5b20c1f4c5cc32ccc8b0
Mos3aB696 <mos3ab696@gmail.com>
2023-09-23 16:07:41 +0300

    upload in third deadline

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,8 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 void set_string(char **s, char *to);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif /* MAIN_H */
