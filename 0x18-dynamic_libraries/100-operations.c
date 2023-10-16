14fffe6a9cea0e47a7daccc81b325f592cb4beb1
Mos3aB696 <mos3ab696@gmail.com>
2023-10-16 23:05:19 +0300

    task one in advanced

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+/**
+ * add - add two numbers
+ * @a: first arg
+ * @b: first arg
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - substraction two numbers
+ * @a: first arg
+ * @b: first arg
+ *
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication two numbers
+ * @a: first arg
+ * @b: first arg
+ *
+ * Return: a + b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division two numbers
+ * @a: first arg
+ * @b: first arg
+ *
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - moduols two numbers
+ * @a: first arg
+ * @b: first arg
+ *
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
